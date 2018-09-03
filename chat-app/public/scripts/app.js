978261bbe43b2e4a4bade764a34c6d678129672a
dragosbus <dragosbusuioc94@gmail.com>
2018-09-03 15:46:40 +0200

    post new message

--- chat-app/public/scripts/app.js
+++ chat-app/public/scripts/app.js
@@ -1,18 +1,29 @@
 const Main = (function () {
-    let form, nameInput, messagesDiv;
+    let form, nameInput, messagesDiv, messageInput;
     const nameColors = ['rgb(12, 90, 206)', 'rgb(82, 16, 168)', 'rgb(179, 3, 149)', 'rgb(31, 190, 58)'];
 
     const init = () => {
         form = document.getElementById('form');
-        nameInput = document.querySelector('#form input');
+        nameInput = document.getElementById('name');
         messagesDiv = document.getElementById('messages');
+        messageInput = document.querySelector('#form input');
 
         form.addEventListener('submit', e => {
             e.preventDefault();
-            messagesDiv.innerHTML += addMessage({
-                name: 'dragos',
-                message: 'Hello'
-            })
+            fetch('http://localhost:3000', {
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: nameInput.value,
+                        message: messageInput.value
+                    })
+                }).then(res => res.json())
+                .then(data => {
+                    messagesDiv.innerHTML += addMessage(data[data.length - 1]);
+                })
+                .catch(err => alert(err));
         });
 
         fetchMessages();
--- chat-app/server.js
+++ chat-app/server.js
@@ -1,25 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
 
 const messages = [{
     name: 'dragos',
     message: 'hello'
-},{
-    name:'mihail',
+}, {
+    name: 'mihail',
     message: 'hi'
 }];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/messages', (req,res)=>{
-    if(messages.length) {
+app.get('/messages', (req, res) => {
+    if (messages.length) {
         res.status(200).json(messages);
     } else {
-        res.status(404).send({error: 'Not messages avaible'});
+        res.status(404).send({
+            error: 'Not messages avaible'
+        });
     }
 });
 
-app.listen(3000, ()=>{
+app.post('/', (req, res) => {
+    const {
+        name,
+        message
+    } = req.body;
+    if (!name || !message) {
+        res.status(404).send({
+            error: 'Must type the name or the message'
+        });
+    } else {
+        messages.push({
+            name,
+            message
+        });
+        res.status(200).json(messages);
+    }
+});
+
+app.listen(3000, () => {
     console.log('Listen on 3000');
 });
