286520ec3aee9407122a4c99b56ed3045ba5e2b0
dragosbus <dragosbusuioc94@gmail.com>
2018-09-10 14:17:43 +0200

    init chat app with socjet.io

---
+++ node+express/lecture/public/chat.js
@@ -0,0 +1,5 @@
+(function () {
+    const nameVal = document.getElementById('name');
+    const messageVal = document.getElementById('message');
+    const chatApp = document.querySelector('.chat');
+}());
